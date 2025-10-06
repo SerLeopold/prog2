951205cdc2f4cc94d6a203c0f55cba25c710fe46
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-10-06 11:34:54 +0200

    Add files via upload

---
+++ kedvencfuggveny.java
@@ -0,0 +1,30 @@
+public class kedvencfuggveny {
+    public static void main(String[] args) {
+    
+    //1.   
+    double r = Math.random();
+    System.out.println(r);
+    //véletlen szám 0 és 1 között pszeduovéletlen, algoritmus alapján számolja ki.
+    
+    //2
+    double s = Math.sin(Math.PI / 2);
+        System.out.println(s);
+    //a szög szinuszát adja vissza. a szög radiánba van megadva
+    
+    //3
+        System.out.println(Math.round(3.6));
+    //kerekíti a számot a legközelebbi egészre
+    
+    //4
+        System.out.println(Math.hypot(3, 4));
+    //kiszámolja a derékszögű háromszög átlóját (sqrt(a*a + b*b))
+    
+    //5 
+        System.out.println(Math.cbrt(27));
+    //egy szám köbgyökét számolja ki, pl. térfogatból visszakapjuk a kocka élhosszát
+    }
+    
+    
+    
+    
+}
